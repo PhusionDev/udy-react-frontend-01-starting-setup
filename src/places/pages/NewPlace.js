5c6868d1a10cb541c93221b42ce035299318e989
Jared Hodgkins <PhusionDev@gmail.com>
2022-11-17 15:19:53 -0500

    61. Sharing Input Values and Adding Multiple Inputs

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import './NewPlace.css';
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from '../../shared/util/validators';
 import Input from '../../shared/components/FormElements/Input';
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form action='' className='place-form'>
       <Input
+        id='title'
+        element='input'
         type='text'
         label='Title'
         validators={[VALIDATOR_REQUIRE()]}
-        errorText='Please Enter a Valid Title'
-        element='input'
+        errorText='Please enter a valid title.'
+        onInput={titleInputHandler}
+      />
+      <Input
+        id='description'
+        element='textarea'
+        label='Description'
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText='Please enter a valid description(at least 5 characters).'
+        onInput={descriptionInputHandler}
       />
     </form>
   );
