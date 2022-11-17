e7e9b8aafc45d858664ac1986c09ae4c9b666fcf
Jared Hodgkins <PhusionDev@gmail.com>
2022-11-17 14:42:20 -0500

    59. Managing State in the Input Component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,13 @@ import Input from '../../shared/components/FormElements/Input';
 const NewPlace = () => {
   return (
     <form action='' className='place-form'>
-      <Input type='text' label='Title' element='input' />
+      <Input
+        type='text'
+        label='Title'
+        validators={[]}
+        errorText='Please Enter a Valid Title'
+        element='input'
+      />
     </form>
   );
 };
